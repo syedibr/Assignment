5e8d586d95f7801dca7189d9cb9a3a77436f8784
syedibr <92871985+syedibr@users.noreply.github.com>
2021-11-15 13:11:51 +0530

    Create junit test.java

---
+++ junit/junit test.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JUnitTest {
+
+	@Test
+	void test() {
+		System.out.println("This test Run");
+	}
+
+}
