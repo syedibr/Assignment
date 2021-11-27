1242c10210d5aacd65bb9c7d08e112bc4725a400
syedibr <92871985+syedibr@users.noreply.github.com>
2021-11-27 14:41:58 +0530

    Create hrrpclient.java

---
+++ java 11_6/hrrpclient.java
@@ -0,0 +1,24 @@
+package Java_11_6;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Hrrpclient {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		 String url = "http://httpbin.org/get";
+
+	        var request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
+
+	        var client = HttpClient.newHttpClient();
+
+	        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+	        System.out.println("Status Code: " + response.statusCode());
+	        System.out.println("Response: " + response.body());
+	}
+
+}
